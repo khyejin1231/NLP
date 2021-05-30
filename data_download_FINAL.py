73b2e466874c0fe5d4f9c00c8d26a5efadb1604a
HyeJin Kim <hyejin.k1231@gmail.com>
2021-05-30 13:28:48 +0900

    data with h_index

--- data_download_FINAL.py
+++ data_download_FINAL.py
@@ -8,6 +8,14 @@ We are interested in title, keywords and abstract
 from pybliometrics.scopus import ScopusSearch
 import pandas as pd
 from pybliometrics.scopus import AuthorRetrieval
+from scholarly import scholarly
+from scholarly import scholarly, ProxyGenerator
+
+pg = ProxyGenerator()
+pg.FreeProxies()
+scholarly.use_proxy(pg)
+
+
 # from pybliometrics.scopus import SerialTitle
 # from pybliometrics.scopus import exception
 # from multiprocessing import Pool
@@ -23,10 +31,44 @@ search_result = ScopusSearch("(TITLE-ABS-KEY(machine learning) \
                                AND TITLE-ABS-KEY(marketing))\
                                    OR (TITLE-ABS-KEY(neural network) \
                                AND TITLE-ABS-KEY(marketing))")
+                                     
+search_result2 = ScopusSearch("(TITLE-ABS-KEY(machine learning) \
+                             AND TITLE-ABS-KEY(consumer)) \
+                              OR (TITLE-ABS-KEY(artificial intelligence) \
+                               AND TITLE-ABS-KEY(consumer))\
+                       OR (TITLE-ABS-KEY(deep learning) \
+                               AND TITLE-ABS-KEY(consumer))\
+                                   OR (TITLE-ABS-KEY(neural network) \
+                               AND TITLE-ABS-KEY(consumer))")                                       
+
+search_result3 = ScopusSearch("(TITLE-ABS-KEY(lasso regression) \
+                               AND TITLE-ABS-KEY(marketing))\
+                                 OR (TITLE-ABS-KEY(augmented reality) \
+                               AND TITLE-ABS-KEY(marketing))\
+                                 OR (TITLE-ABS-KEY(churn prediction) \
+                               AND TITLE-ABS-KEY(marketing))\
+                                 OR (TITLE-ABS-KEY(reinforcement learning) \
+                               AND TITLE-ABS-KEY(marketing))")
+                                     
+search_result4 = ScopusSearch("(TITLE-ABS-KEY(lasso regression) \
+                               AND TITLE-ABS-KEY(consumer))\
+                                 OR (TITLE-ABS-KEY(augmented reality) \
+                               AND TITLE-ABS-KEY(consumer))\
+                                 OR (TITLE-ABS-KEY(churn prediction) \
+                               AND TITLE-ABS-KEY(consumer))\
+                                 OR (TITLE-ABS-KEY(reinforcement learning) \
+                               AND TITLE-ABS-KEY(consumer))")
 
 print("Documents found:", search_result.get_results_size())
 
-data = pd.DataFrame(search_result.results)
+df1 = pd.DataFrame(search_result.results)
+df2 = pd.DataFrame(search_result2.results)
+df3 = pd.DataFrame(search_result3.results)
+df4 = pd.DataFrame(search_result4.results)
+data = pd.concat([df1, df2, df3, df4])
+data.drop_duplicates(subset ="title",
+                     keep = False, inplace = True)
+df = data
 
 # Articles only; no conference paper or books or others
 df = data.loc[data['subtypeDescription'].isin(['Article'])]
@@ -46,28 +88,31 @@ df = df[(df['coverDate'] <= "2020-06-01")]
 title = df['title']
 abstract = df['description']
 
-author = df['author_ids']
+author = df.author_names.str.split(';', expand=True)
+year = pd.DatetimeIndex(df['coverDate']).year
+year = pd.DataFrame(year)
+author = pd.concat([year, author.reset_index(drop=True)], axis = 1)
+result = []
+author[0][1]
 
-first_author = author.astype(str).str[0:11]
-first_author = first_author.replace({';':''}, regex = True)
+for i in range(1672, len(author)):
+    print(i)
+    year = author['coverDate'][i] - 1
+    try:
+        search_query = scholarly.search_author(author[0][i])
+        author1 = next(search_query)
+        result1 = scholarly.fill(author1, sections=['counts']).get('cites_per_year').get(year)
+        result.append(result1)
+    except StopIteration:
+        result.append(None)
+    except TypeError:
+        result.append(None)
+        
 
-second_author = author.astype(str).str[12:23]
-
-# convert first_author to dataframe
-first_author = pd.DataFrame(first_author) # Create a new column of None
-first_author["h_index"] = [None] * first_author.shape[0]    
-
-for i in range(len(df)):
-        print(i)
-        try:
-            first_author.iloc[i,1] = int(AuthorRetrieval(first_author.iloc[i,0]).h_index)
-    # If a ValueError expection is raised, we put a np.nan
-        except ValueError:
-            first_author.iloc[i,1] = pd.NA
-    # If a TypeError expection is raised, we put a np.nan
-        except TypeError:
-            first_author.iloc[i,1] = pd.NA
-            
+#654 it stopped
+#1071 it stopped
+#1285 it stopped 
+#1672
 # =============================================================================
 # # Loop to populate the columns h_index with the h-index dowloaded from Scopus
 # if __name__ == '__main__':
@@ -80,8 +125,8 @@ for i in range(len(df)):
 df = pd.merge(df, first_author.h_index, left_index=True, right_index=True)
 
 # Check the Nan
-df[pd.isna(df.h_index)]
-df = df[df.h_index.notna()]
+#df[pd.isna(df.h_index)]
+#df = df[df.h_index.notna()]
 
 # Convert issn column to dataframe
 df = df[df['issn'].notna()]
@@ -113,5 +158,5 @@ SJR = pd.DataFrame(df.issn)
 # df = df[df['author_ids'].notna()]
 # =============================================================================
 
-# df.to_pickle("C:/Users/USER/Documents/Python/NLP/extracted_data.pkl")
-df.to_pickle(r"C:\Users\marco\Desktop\NLP\extracted_data.pkl")
+df.to_pickle("C:/Users/USER/Documents/Python/NLP/extracted_data.pkl")
+#df.to_pickle(r"C:\Users\marco\Desktop\NLP\extracted_data.pkl")
